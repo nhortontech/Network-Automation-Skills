4825b252bfa3a6e7d05acee8d625b02140451260
nhortontech <nhortontech@gmail.com>
2026-08-24 05:17:00 +0000

    chore: update infracost.tf with isolated work script

--- infracost.tf
+++ infracost.tf
@@ -1,3 +1,34 @@
+# infracost.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # These parameters force the Azure provider to bypass live tenant validation
+  skip_provider_registration = true
+  
+  # Crucial for offline testing: skips checking if the resource group exists live in Azure
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+
+
+
+
+
+
+# FOR USE AT HOME:
+
+/*
 provider "azurerm" {
   features {}
 }
@@ -30,3 +61,8 @@ resource "azurerm_linux_virtual_machine" "test_vm" {
     version   = "latest"
   }
 }
+*/
+
+
+
+
